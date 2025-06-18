9947349bbb6d2252ef297c6d614c378627119870
Aisha Buzu <137095439+aishabuzu@users.noreply.github.com>
2025-06-18 16:32:16 +0300

    models.py

--- sacco_managment/financial_services/models.py
+++ sacco_managment/financial_services/models.py
@@ -59,7 +59,7 @@ class CryptoTransaction(models.Model):
     amount = models.DecimalField(
         max_digits=20, 
         decimal_places=8,
-        validators=[MinValueValidator(Decimal('0.00000001'))]
+        validators=[MinValueValidator(Decimal('0.000000001'))]
     )
     status = models.CharField(
         max_length=10, 
